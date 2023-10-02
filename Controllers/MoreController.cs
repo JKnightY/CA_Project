9eb823731a28c588befda00719e40f479bb47c1f
JinKeyi <e1133134@u.nus.edu>
2023-10-02 17:22:53 +0800

    first commit

---
+++ Controllers/MoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CA_Project.Controllers
+{
+    public class MoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
